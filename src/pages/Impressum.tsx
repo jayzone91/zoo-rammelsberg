30fe50685874a74a9c7c49606ecc46ef2253fd90
jayzone91 <register@j4yz0n3.de>
2023-09-06 18:20:06 +0200

    Update Impressum.tsx

--- src/pages/Impressum.tsx
+++ src/pages/Impressum.tsx
@@ -1,6 +1,9 @@
+import { Container } from "react-bootstrap";
 import useTitle from "../hooks/useTitle";
 
 export default function Impressum() {
   useTitle("Impressum");
-  return <div>Impressum</div>;
+  return <Container>
+    <h1 className="text-center">Impressum</h1>
+  </Container>;
 }
